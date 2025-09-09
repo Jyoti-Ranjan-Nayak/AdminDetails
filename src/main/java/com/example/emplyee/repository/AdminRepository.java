87b601582b4b2f1799529c101282f362e7df351a
prakash210697 <97579045+prakash210697@users.noreply.github.com>
2025-09-09 14:21:34 +0530

    second commit

--- src/main/java/com/example/emplyee/repository/AdminRepository.java
+++ src/main/java/com/example/emplyee/repository/AdminRepository.java
@@ -9,7 +9,7 @@ import com.example.emplyee.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     //Admin findByUsername(String username);
-	 Optional<Admin> findByUsername(String username);
+	 Optional<Admin>  findByUsername(String username);
 	 List<Admin> findByUsernameContainingIgnoreCase(String username);
 
 }
